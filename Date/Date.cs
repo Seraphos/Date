6e59f3f7d7422d19683389b87cde645c631d0294
Andrew Connelly <andrew.connelly@spgusa.com>
2018-06-27 14:01:19 -0400

    Add project files.

---
+++ Date/Date.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Date
+{
+    public class Date
+    {
+        private int month, day, year;
+
+        public int Month
+        {
+            get { return month; }
+            set
+            {
+                if (month <= 0) throw new ArgumentOutOfRangeException("Cannot enter a negative number.");
+                else month = value;
+            }
+        }//end month property
+
+        public int Day
+        {
+            get { return day; }
+            set
+            {
+                if (day <= 0) throw new ArgumentOutOfRangeException("Cannot enter a negative number.");
+                else day = value;
+            }
+        }//end day property
+              
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (year <= 0) throw new ArgumentOutOfRangeException("Cannot enter a negative number.");
+                else year = value;
+            }
+        }//end year property
+
+        public Date(int consMonth,int consDay,int consYear) {
+            month = consMonth;
+            day = consDay;
+            year = consYear;
+        }//end constructor
+        
+        public string DisplayDate(int paramMonth, int paramDay, int paramYear) {
+            int methMonth, methDay, methYear;
+
+            methMonth = month; methDay = day; methYear = year;
+            string strMonth = Convert.ToString(methMonth); string strDay = Convert.ToString(methDay); string strYear = Convert.ToString(methYear);
+            string FullDate = strMonth + '/' + strDay + '/' + strYear;
+            return FullDate;
+            
+        }//end method
+
+    }//end class
+}//end namespace
---
+++ Date/DateTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Date
+{
+    class DateTest
+    {
+        static void Main(string[] args)
+        {
+            int month, day, year;
+            Console.WriteLine("Please enter a day, month, and year to be displayed by a method: ");
+            Console.WriteLine("Day: "); day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Month: "); month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Year: "); year = Convert.ToInt32(Console.ReadLine());
+
+            Date myDate = new Date(month, day, year);
+            Console.WriteLine("The date formatted is {0}", myDate.DisplayDate(month, day, year));
+            Console.ReadKey();
+        }
+    }
+}
